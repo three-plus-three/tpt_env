cc33332d9af2a5145e6c4c212c729fd02adb890c
runner.mei <runner.mei@gmail.com>
2016-04-29 12:10:05 +0800

    update postgresql to v9.5.2

--- postgresql/pg_env.bat
+++ postgresql/pg_env.bat
@@ -1,11 +1,9 @@
 @ECHO OFF
 REM The script sets environment variables helpful for PostgreSQL
 
-@SET PATH="%~dp0bin";%PATH%
-@SET PGDATA=%~dp0..\..\data\pg
+@SET PATH="%~dp0";%PATH%
+@SET PGDATA=%~dp0..\..\data
 @SET PGDATABASE=postgres
 @SET PGUSER=postgres
 @SET PGPORT=35432
 @SET PGLOCALEDIR=%~dp0share\locale
-
-                          
