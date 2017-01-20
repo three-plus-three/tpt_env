929ee2cd2379fed74b9c04195bcd85736af9a1ee
eeiceyes <eeiceyes@163.com>
2017-01-20 11:10:32 +0800

    add flyway cmd

---
+++ flyway/flyway.cmd
@@ -0,0 +1,29 @@
+@REM
+@REM Copyright 2010-2016 Boxfuse GmbH
+@REM
+@REM Licensed under the Apache License, Version 2.0 (the "License");
+@REM you may not use this file except in compliance with the License.
+@REM You may obtain a copy of the License at
+@REM
+@REM         http://www.apache.org/licenses/LICENSE-2.0
+@REM
+@REM Unless required by applicable law or agreed to in writing, software
+@REM distributed under the License is distributed on an "AS IS" BASIS,
+@REM WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+@REM See the License for the specific language governing permissions and
+@REM limitations under the License.
+@REM
+
+@Echo off
+
+setlocal
+
+@REM Set the current directory to the installation directory
+set INSTALLDIR=%~dp0
+
+set JAVA_CMD="..\..\runtime_env\jre\bin\java.exe"
+
+%JAVA_CMD% -cp "%INSTALLDIR%\lib\*;%INSTALLDIR%\drivers\*" org.flywaydb.commandline.Main %*
+
+@REM Exit using the same code returned from Java
+EXIT /B %ERRORLEVEL%
